391f78e4753267c37a094bfd6760fa8ddeb33494
Roman Korchagin <r-korchagin@yandex.ru>
2019-04-18 15:26:01 +0300

    The Love-Letter Mystery

---
+++ TheLoveLetterMystery.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// hackerrank question
+// The Love-Letter Mystery
+
+function theLoveLetterMystery(s) {
+    let changeCount = 0;
+    let len = s.length;
+    for(let i=0; i<parseInt(len/2); i++){
+      changeCount += Math.abs(s.charCodeAt(i) - s.charCodeAt(len-i-1))
+    }
+    return changeCount;
+  }
+  
+  console.log(theLoveLetterMystery("abc")); // Expected 2
+  console.log(theLoveLetterMystery("abcba")); // Expected 0
+  console.log(theLoveLetterMystery("abcd")); // Expected 4
