ef42ef734385712338078ea0cd1c16ee7abab52a
Roman Korchagin <r-korchagin@yandex.ru>
2019-04-19 14:45:47 +0300

    Jumping on the Clouds

---
+++ JumpingOnTheClouds.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// hackerrank question
+// Jumping on the Clouds
+
+function jumpingOnClouds(c) {
+    let jumpCount = 0;
+    for ( let i=0; i<c.length; ){
+       if (c[i+1] === 0 && c[i+2] === 0) {i+=2; jumpCount++;}
+       else if (c[i+1] === 1 && c[i+2] === 0) {i+=2; jumpCount++;}
+       else if (c[i+1] === 0) {i++; jumpCount++;}
+       else {i++};
+    }
+       return jumpCount;
+   }
+   
+   console.log(jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])); // Expected 4
